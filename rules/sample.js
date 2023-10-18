858cbb474fcc5409e0a9e7365050d1f096cfefb0
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-10-18 09:08:05 -0500

    Bump reng to the latest version so that metadata is available
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- rules/sample.js
+++ rules/sample.js
@@ -3,7 +3,7 @@
  * the root README.md file.
  */
 
-function main(inp) {
+function main(inp, _metadata) {
   const numPatches = inp.length;
   if (numPatches == 0) {
     // No files updated, so approve.

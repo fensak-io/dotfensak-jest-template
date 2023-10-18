acf0239a095d7318f400db5c48c770bb0beecd81
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-10-18 09:08:05 -0500

    Bump reng to the latest version so that metadata is available
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- rules/sample.test.js
+++ rules/sample.test.js
@@ -25,7 +25,7 @@ const testRepo = {
 const opts = { logMode: RuleLogMode.Console };
 
 test("No changes should be approved", async () => {
-  const result = await runRule(ruleFn, [], opts);
+  const result = await runRule(ruleFn, [], { sourceBranch: "foo" }, opts);
   expect(result.approve).toBe(true);
 });
 
@@ -33,7 +33,12 @@ test("Changes only to README should be approved", async () => {
   // View PR at
   // https://github.com/fensak-io/dotfensak-deno-template/pull/1
   const patches = await patchFromGitHubPullRequest(octokit, testRepo, 1);
-  const result = await runRule(ruleFn, patches.patchList, opts);
+  const result = await runRule(
+    ruleFn,
+    patches.patchList,
+    patches.metadata,
+    opts,
+  );
   expect(result.approve).toBe(true);
 });
 
@@ -41,7 +46,12 @@ test("Changes to non-README files should be rejected", async () => {
   // View PR at
   // https://github.com/fensak-io/dotfensak-deno-template/pull/2
   const patches = await patchFromGitHubPullRequest(octokit, testRepo, 2);
-  const result = await runRule(ruleFn, patches.patchList, opts);
+  const result = await runRule(
+    ruleFn,
+    patches.patchList,
+    patches.metadata,
+    opts,
+  );
   expect(result.approve).toBe(false);
 });
 
@@ -49,6 +59,11 @@ test("Change containing more than one file should be rejected", async () => {
   // View PR at
   // https://github.com/fensak-io/dotfensak-deno-template/pull/4
   const patches = await patchFromGitHubPullRequest(octokit, testRepo, 4);
-  const result = await runRule(ruleFn, patches.patchList, opts);
+  const result = await runRule(
+    ruleFn,
+    patches.patchList,
+    patches.metadata,
+    opts,
+  );
   expect(result.approve).toBe(false);
 });
